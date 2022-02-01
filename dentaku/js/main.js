d2009cc733842897cec8694a761905bba9cda68c
hayato0413 <82205450+hayato0413@users.noreply.github.com>
2022-02-01 18:49:20 +0900

    Add files via upload

---
+++ dentaku/js/main.js
@@ -0,0 +1,37 @@
+　let myTotal = 0;
+  let myInput = "";
+  let myFlg = 1;
+  
+  function myValue(myData) {
+    if (myData == 0 || myData == '00' || myData == '.') {
+      if (myTotal !== 0 || myFlg == 0) {
+        myInput += myData;
+        document.dentaku.myLine.value = myInput;
+      }
+    } else {
+      myInput += myData;
+      myFlg = 0;
+      document.dentaku.myLine.value = myInput;
+    }
+  }
+  
+  function myCalculate(mySign) {
+    if (myFlg == 0 && mySign !== '=') {
+      myInput += mySign;
+      document.dentaku.myLine.value = myInput;
+      myFlg = 1;
+    }
+    if (mySign == '='){
+      myTotal = eval(myInput);
+      myInput = myTotal;
+      myFlg = 0;
+      document.dentaku.myLine.value = myTotal;
+    }
+  }
+      
+  function myC() {
+    myTotal = 0;
+    myInput = "";
+    myFlg = 1;
+    document.dentaku.myLine.value = myTotal;
+  }
